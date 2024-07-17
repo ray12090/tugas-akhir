68a8642342b78beb7f7f7f594d80db08e08d9fd2
Pey <sophiaraudhah@gmail.com>
2024-07-17 12:38:37 +0700

    add countries detailKewarganegaraan

---
+++ app/Http/Controllers/DetailTempatLahirController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\detailTempatLahir;
+use Illuminate\Http\Request;
+
+class DetailTempatLahirController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(detailTempatLahir $detailTempatLahir)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(detailTempatLahir $detailTempatLahir)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, detailTempatLahir $detailTempatLahir)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(detailTempatLahir $detailTempatLahir)
+    {
+        //
+    }
+}
