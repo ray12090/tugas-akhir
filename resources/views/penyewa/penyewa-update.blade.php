1e32390ffd61cd35f0c33f0836010000af2986df
Achmad Fahresya Rayhannur <af.rayhannur@gmail.com>
2024-08-02 00:51:17 +0700

    pemilik penyewa ui update

--- resources/views/penyewa/penyewa-update.blade.php
+++ resources/views/penyewa/penyewa-update.blade.php
@@ -196,7 +196,7 @@
                                             {{ __('Jenis Kelamin') }}
                                         </label>
                                         <ul
-                                            class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                            class="w-auto text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                                             <li
                                                 class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                                                 <div class="flex items-center ps-3">
