0c05c6251d20bd335310393152a81cbcc5fcf3d9
Pey <sophiaraudhah@gmail.com>
2024-08-02 01:35:05 +0700

    Update view pemilik penyewa (alamat sesuai ktp->jalan)

--- resources/views/pemilik/pemilik-read.blade.php
+++ resources/views/pemilik/pemilik-read.blade.php
@@ -151,7 +151,7 @@
                             </div>
                             <div class="sm:col-span-2">
                                 <label for="alamat"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Alamat Sesuai KTP') }}</label>
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Jalan') }}</label>
                                 <div class="relative">
                                     <p
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
--- resources/views/penyewa/penyewa-create.blade.php
+++ resources/views/penyewa/penyewa-create.blade.php
@@ -272,7 +272,7 @@
                                 </div>
                                 <div class="sm:col-span-2">
                                     <label for="alamat"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Alamat Sesuai KTP') }}</label>
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ __('Jalan') }}</label>
                                     <div class="relative">
                                         <textarea id="alamat" name="alamat" rows="4"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"></textarea>
