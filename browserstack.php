c0974ef9774c48a2d019aee3157fda36489767ce
Pey <sophiaraudhah@gmail.com>
2024-08-03 23:10:48 +0700

    dashboard pemilik & browserstack

---
+++ browserstack.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'c_url' => env('BROWSERSTACK_CONNECTION_URL')
+];
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -66,7 +66,7 @@
                     @if (Auth::user()->tipe_user_id == 11)
                         @if (App\Models\Pemilik::where('user_id', Auth::user()->id)->exists())
                             <div
-                                class="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-4 rounded-lg p-6 shadow-md sm:rounded-2xl bg-[#01645222]">
+                                class="grid gap-4 sm:gap-6 grid-cols-1 sm:grid-cols-3 rounded-lg p-6 shadow-md sm:rounded-2xl bg-[#01645222]">
                                 <a href=""
                                     class="flex flex-col items-center justify-center rounded-lg border border-gray-200 bg-[#016452] px-4 py-6 hover:bg-[#014F41] dark:border-[#014F41] dark:bg-gray-800 dark:hover:bg-[#014F41]">
                                     <svg class="w-[48px] h-[48px] text-white dark:text-white" aria-hidden="true"
@@ -76,18 +76,7 @@
                                             d="M4 4a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2v14a1 1 0 1 1 0 2H5a1 1 0 1 1 0-2V5a1 1 0 0 1-1-1Zm5 2a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H9Zm5 0a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-1Zm-5 4a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1H9Zm5 0a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 0 0-1-1h-1Zm-3 4a2 2 0 0 0-2 2v3h2v-3h2v3h2v-3a2 2 0 0 0-2-2h-2Z"
                                             clip-rule="evenodd" />
                                     </svg>
-                                    <span class="text-sm font-medium text-white">{{ __('Data Unit') }}</span>
-                                </a>
-                                <a href="#"
-                                    class="flex flex-col items-center justify-center rounded-lg border border-gray-200 bg-[#016452] px-4 py-6 hover:bg-[#014F41] dark:border-[#014F41] dark:bg-gray-800 dark:hover:bg-[#014F41]">
-                                    <svg class="w-[48px] h-[48px] text-white dark:text-white" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        fill="currentColor" viewBox="0 0 24 24">
-                                        <path fill-rule="evenodd"
-                                            d="M9 15a6 6 0 1 1 12 0 6 6 0 0 1-12 0Zm3.845-1.855a2.4 2.4 0 0 1 1.2-1.226 1 1 0 0 1 1.992-.026c.426.15.809.408 1.111.749a1 1 0 1 1-1.496 1.327.682.682 0 0 0-.36-.213.997.997 0 0 1-.113-.032.4.4 0 0 0-.394.074.93.93 0 0 0 .455.254 2.914 2.914 0 0 1 1.504.9c.373.433.669 1.092.464 1.823a.996.996 0 0 1-.046.129c-.226.519-.627.94-1.132 1.192a1 1 0 0 1-1.956.093 2.68 2.68 0 0 1-1.227-.798 1 1 0 1 1 1.506-1.315.682.682 0 0 0 .363.216c.038.009.075.02.111.032a.4.4 0 0 0 .395-.074.93.93 0 0 0-.455-.254 2.91 2.91 0 0 1-1.503-.9c-.375-.433-.666-1.089-.466-1.817a.994.994 0 0 1 .047-.134Zm1.884.573.003.008c-.003-.005-.003-.008-.003-.008Zm.55 2.613s-.002-.002-.003-.007a.032.032 0 0 1 .003.007ZM4 14a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0v-4a1 1 0 0 1 1-1Zm3-2a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1Zm6.5-8a1 1 0 0 1 1-1H18a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-.796l-2.341 2.049a1 1 0 0 1-1.24.06l-2.894-2.066L6.614 9.29a1 1 0 1 1-1.228-1.578l4.5-3.5a1 1 0 0 1 1.195-.025l2.856 2.04L15.34 5h-.84a1 1 0 0 1-1-1Z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                    <span class="text-sm font-medium text-white">{{ __('Data IPL') }}</span>
+                                    <span class="text-sm font-medium text-white">{{ __('Unit dan Tagihan') }}</span>
                                 </a>
                                 <a href="#"
                                     class="flex flex-col items-center justify-center rounded-lg border border-gray-200 bg-[#016452] px-4 py-6 hover:bg-[#014F41] dark:border-[#014F41] dark:bg-gray-800 dark:hover:bg-[#014F41]">
